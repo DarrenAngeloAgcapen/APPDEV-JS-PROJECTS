718503ec6ceb7b33b771d3f1ae64dc58ddbfac30
DarrenAngeloAgcapen <97968802+DarrenAngeloAgcapen@users.noreply.github.com>
2024-08-21 16:18:56 +0800

    Add files via upload

--- PRELIMS/JSPROJECTS/QuoteGenerator/quote.js
+++ PRELIMS/JSPROJECTS/QuoteGenerator/quote.js
@@ -2,7 +2,10 @@ const quotes= ["Tolerance and Apathy are the last virtues of a dying society. -A
 "Anyone who holds a true opinion without understanding is like a blind man on the right road. -Socrates",
 "It is our choice of good or evil that determines our character, not our opinion about good or evil. -Aristotle",
 "The only thing I know is that I know nothing. -Socrates",
-"He who is not satisfied with a little, is satisfiedwith nothing. -Epicurus"]
+"He who is not satisfied with a little, is satisfied with nothing. -Epicurus",
+"The only source of knowledge is experience. -Albert Einstein",
+"Every man is guilty of all the good he didn't do. -Voltaire",
+"What worries you, masters you. -John Locke"]
 
 
 const btn= document.getElementById("btn")
