e6a940d21edbbd76a0545a88a439240a0254ab25
DarrenAngeloAgcapen <97968802+DarrenAngeloAgcapen@users.noreply.github.com>
2024-09-11 15:34:59 +0800

    Delete PRELIMS/JSPROJECTS/personal-profile SRC/src/src directory

--- PRELIMS/JSPROJECTS/personal-profile SRC/src/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer(){
-    return(
-        <footer>
-            <p>
-                &copy; {new Date().getFullYear()} My Personal Profile || Written By: Agcapen, Darren Angelo
-            </p>
-        </footer>
-    )
-}
