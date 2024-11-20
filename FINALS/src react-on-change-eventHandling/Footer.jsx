3dbd22a6eeed305ad2d973b07796d4811eb4f6c8
DarrenAngeloAgcapen <97968802+DarrenAngeloAgcapen@users.noreply.github.com>
2024-11-20 15:36:43 +0800

    Add files via upload

---
+++ FINALS/src react-on-change-eventHandling/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+export default function Footer() {
+
+    return (
+        <footer>
+            <hr />
+            <p>&copy; {new Date().getFullYear()} My Website|| Written by: Agcapen, Darren Angelo</p>
+        </footer>
+    )
+}
